515229debb3b53a54af9c35fc386bbc9156b000c
Viktor Borisov <vik.borisov@outlook.com>
2017-05-17 01:37:58 +0700

    was modified a TlProcessor module

--- src/TelegramClient.Entities.Generator/TlProcessor.fs
+++ src/TelegramClient.Entities.Generator/TlProcessor.fs
@@ -1,6 +1,27 @@
 module TlProcessor
 
-open Models;
+open Models
+open System.Text.RegularExpressions
+
+let isCommented (line: string) =
+    match line.StartsWith("//") with
+    | true -> Some()
+    | false -> None
+
+let isNotCommented (line: string) =
+    match line.StartsWith("//") with
+    | true -> Some()
+    | false -> None
+
+let parseMethod (line: string, schema: Schema) =
+    let matches = Regex.Matches(line, "^([a-zA-Z\.\_]*)#([0-9a-z]*) (.*) = (.*);$")
+    
 
 let Parse (schema:string[]) =
+
+    for line in schema do
+        match isCommented line with
+        | Some(_) -> ()
+        | None -> 
+
     ()
